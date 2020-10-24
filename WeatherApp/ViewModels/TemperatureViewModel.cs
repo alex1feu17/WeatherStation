f9389d8e86658de4f2eca4b74d9be4536266ff31
alex1feu17 <alexyd@hotmail.ca>
2020-10-24 18:35:53 -0400

    T01 CelsisInFahrenheit_AlwaysReturnGoodValue : Done

---
+++ WeatherApp/ViewModels/TemperatureViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeatherApp.ViewModels
+{
+    public class TemperatureViewModel : BaseViewModel
+    {
+        /// TODO : Ajoutez le code nécessaire pour réussir les tests et répondre aux requis du projet
+        public object ConvertCelsiusInFahrenheit(double expected)
+        {
+            var actual = expected - 32;
+            actual = actual * (5 / 9);
+            return actual;
+        }
+    }
+}
